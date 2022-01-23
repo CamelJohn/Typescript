8d8554d6115406aceaf0bfa2ee6eadf5213280a3
Jonathan A <jatia-c@myhippo.com>
2022-01-23 14:17:52 +0200

    tuples

---
+++ src/tuples.ts
@@ -0,0 +1,10 @@
+type asyncResponse = { data?: any; error: Error | null };
+
+type handlerResponse = [Error | null, any | null];
+
+function errorHandler(cb: () => asyncResponse): handlerResponse {
+  const response = cb();
+
+    return [response.error, response.data];
+
+}
