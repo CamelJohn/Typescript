f76219619008ffa5c2efd0c56bc556c206454da6
Jonathan A <jatia-c@myhippo.com>
2022-01-23 12:38:56 +0200

    removed excess

--- cut.check.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-function cut(array) {
-    if (orray.length > 10) {
-        return array.trim(0, 10);
-    }
-    return array;
-}
--- cut.ts
+++
@@ -1,6 +0,0 @@
-function cutArray(array: string[]) {
-    if (array.length > 10) {
-        return array.slice(0, 10);
-    }
-    return array;
-}
